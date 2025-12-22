fe91a3cfe0e2e7da8101a8f9a24245ff1ef03846
mhshifat <mehedihassanshifat01@gmail.com>
2025-12-22 20:22:25 +0600

    Add NProgress for navigation loading indicators and implement page loaders

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,10 +1,14 @@
 import { PropsWithChildren } from "react";
 import { Toaster } from 'sonner';
+import { NavigationProgress } from "./navigation-progress";
 
 export default function Providers({ children }: PropsWithChildren) {
     return (
         <>
             <Toaster richColors />
+            <NavigationProgress
+                showFullPageLoader={true}
+            />
             {children}
         </>
     );
